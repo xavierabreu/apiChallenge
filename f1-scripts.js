87cf9bb1c07c74de69715b66ed0a036feba338df
xavierabreu <xavierabreu00@gmail.com>
2023-12-14 17:43:48 -0500

    final

--- f1-scripts.js
+++ f1-scripts.js
@@ -1,3 +1,28 @@
-let seasonEight = document.querySelector("ul");
+let resultsContainer = document.getElementById("results");
 
-fetch("http://ergast.com/api/f1/2008/")
+fetch("http://ergast.com/api/f1/current/last/results.json")
+    .then(function (response) {
+        return response.json();
+    })
+    
+    .then(function (json) {
+        let raceResults = json.MRData.RaceTable.Races[0].Results;
+
+        raceResults.forEach(function (result) {
+            let position = result.position;
+            let driver = result.Driver.givenName + " " + result.Driver.familyName;
+            let time = result.Time.time;
+
+            let card = document.createElement("div");
+            card.className = "race-card";
+            card.innerHTML = `
+                <div>
+                    <p>Position: ${position}</p>
+                    <p>Driver: ${driver}</p>
+                    <p>Time: ${time}</p>
+                </div>
+            `;
+            resultsContainer.appendChild(card);
+        });
+    })
+
